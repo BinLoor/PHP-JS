dd22a16e4810cf8a82aaf8f73dfa6506811bb063
Michael <ironbee2002@yandex.ru>
2022-03-30 13:24:46 +0700

    Авторизация + регистрация

--- class/register.php
+++ class/register.php
@@ -1,32 +1,30 @@
 <?php
 	require_once 'user.php';
-	$user = new User();
-	$user->LoadFromSession();
-	
-	if($user->authorize($_POST) === true) {
-		$_SESSION['authorized'] = 1;
+	if (!empty($_POST)) {
+		$user = new User();
+		$user->LoadFromForm($_POST);
 		$user->SaveToSession();
+		$_SESSION['authorized'] = 1;
 		echo '<script>window.location="index.php"</script>' ;
 	}
-?>
-
+?> 
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Авторизация</title>
+		<title>Регистрация</title>
 		<link rel = 'stylesheet' href='style.css'>
 	</head> 
 <body>
     <div id = 'content'>
 		<form action ='' method = 'post'>
-			<h3>Authorizathion</h3>
+			<h3>Register</h3>
+			<input type='text' name='name' placeholder='name'><br>
 			<input type='text' name='login' placeholder='Login'><br>
 			<input type='text' name='pass' placeholder='Password'><br>
 			<input type='submit' name=''>
 		</form>
-		<a href='register.php'>Регистрация</a>
 	</div>
 </body>
 </html>
