3dfda752d432da83e10fcc4942c055d18589135e
Michael <ironbee2002@yandex.ru>
2022-03-30 11:14:10 +0700

    SaveToSession

--- class/index.php
+++ class/index.php
@@ -1,16 +1,9 @@
 <?php 
 header('Content-Type: text/html; charset=utf-8');
-class User {
-	public $name;
-	public $login;
-	private $pass;
-	public function show() {
-		echo '<h3> Имя: '.$this->name.'</h3>';
-		echo '<h3> Логин: '.$this->login.'</h3>';
-	}
-}
+require_once 'user.php';
+
 $user = new User();
-$user->name = 'Michael';
-$user->login = 'admin';
+$user->set('Michael', 'admin', '123');
+$user->SaveToSession();
 $user->show();
 ?>
