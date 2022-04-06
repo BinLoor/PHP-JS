bc2d686d8669c2cb1a23ea4a70b4e23112d13edb
Michael <ironbee2002@yandex.ru>
2022-04-06 12:44:39 +0700

    Наследование список дисциплин

--- n/group.php
+++ n/group.php
@@ -3,6 +3,7 @@ class Group {
 	private $id;
 	private $title;
 	private $students = array();
+	private $disciplines = array();
 	
 	public function __construct($i,$t) {
 		$this->id = $i;
@@ -14,11 +15,18 @@ class Group {
 	public function AddStudent($s) {
 		array_push($this->students, $s);
 	}
+	public function AddDiscipline($d) {
+		array_push($this->disciplines, $d);
+	}
 	public function display() {
-		echo '<h1>Группа'.$this->title.'</h1>';
+		echo '<h1>Группа '.$this->title.'</h1>';
 		echo '<h2>Список студентов</h2>';
 		foreach ($this->students as $student) {
 			$student->display();
+		}
+		echo '<h2>Список дисциплин</h2>';
+		foreach ($this->disciplines as $discipline) {
+			$discipline->display();
 		}
 	}
 }
