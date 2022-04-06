3cee158ac0dc52991aa65e02b0b7cc3c09706908
Michael <ironbee2002@yandex.ru>
2022-04-06 11:13:49 +0700

    Наследование group php

---
+++ n/group.php
@@ -0,0 +1,14 @@
+<?php
+class Group {
+	private $id;
+	private $title;
+	
+	public function __construct($i,$t) {
+		$this->id = $i;
+		$this->title =$t;
+	}
+	public function GetTitle(){
+		return $this->title;
+	}
+}
+?>
---
+++ n/index.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+require_once 'student.php';
+require_once 'group.php';
+
+$g1 = new Group(1, 'И-01');
+$g2 = new Group(1, 'И-02');
+$g3 = new Group(1, 'И-03');
+
+$s1 = new Student('Винокуров','Михаил','Игоревич', $g3);
+$s1->display();
+?>
