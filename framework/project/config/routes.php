fc2ca21baa7634db83a971153dfca6456fc4a6aa
Michael <ironbee2002@yandex.ru>
2022-04-22 12:56:42 +0700

    Глава 96 Задачи 1-3

--- framework/project/config/routes.php
+++ framework/project/config/routes.php
@@ -10,6 +10,8 @@
 		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
 		new Route('/user/:id/', 'user', 'show'),
 		new Route('/user/:id/:key/', 'user', 'info'),
-		new Route('/page/act/', 'page', 'act'),
+		new Route('/page/act/', 'page', 'act'), 
+		new Route('/product/:n/', 'product', 'show'),
+		new Route('/product/all/', 'product', 'all'),
 	];
 ?>
