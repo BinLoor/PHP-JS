3d9ae4d471bad9e2250368660c72ccc512dc7d06
Michael <ironbee2002@yandex.ru>
2022-05-20 13:46:09 +0700

    56 Class Date

--- framework/project/controllers/TestController.php
+++ framework/project/controllers/TestController.php
@@ -5,17 +5,17 @@
 	class TestController extends Controller
 	{
 		public function act1()
-		{
+		{ $this->title = 'Действие act1 контроллера test';
 			echo 'Привет';
 		}
 		
 		public function act2()
-		{
+		{$this->title = 'Действие act2 контроллера test';
 			echo 'Пока';
 		} 
 		
 		public function act3()
-		{
+		{$this->title = 'Действие act3 контроллера test';
 			echo 'Как дела';
 		}
 	}
