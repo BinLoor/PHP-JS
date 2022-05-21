0e8466e86909658c87b2a6ba90bd923cfdbc3a31
Michael <ironbee2002@yandex.ru>
2022-05-21 20:08:01 +0700

    75 Class Hidden

--- CodeMu_56-78/index.php
+++ CodeMu_56-78/index.php
@@ -3,12 +3,13 @@
 	require_once 'Input.php';
 	require_once 'Submit.php';
 	require_once 'Pswrd.php';
+	require_once 'Hidden.php';
 	
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 	
 	echo $form->open();
-		echo (new Input)   ->setAttr('name', 'login');
-		echo (new Pswrd)->setAttr('name', 'passw');
+		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
+		echo (new Input)->setAttr('name', 'year');
 		echo new Submit;
 	echo $form->close();
 ?>
