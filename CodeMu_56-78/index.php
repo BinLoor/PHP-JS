c39783660b4cb411ca2de1f9346a8c1637fa0789
Michael <ironbee2002@yandex.ru>
2022-05-21 20:03:03 +0700

    74 Class Password

--- CodeMu_56-78/index.php
+++ CodeMu_56-78/index.php
@@ -2,11 +2,13 @@
 	require_once 'Form.php';
 	require_once 'Input.php';
 	require_once 'Submit.php';
+	require_once 'Pswrd.php';
 	
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 	
 	echo $form->open();
-		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)   ->setAttr('name', 'login');
+		echo (new Pswrd)->setAttr('name', 'passw');
 		echo new Submit;
 	echo $form->close();
 ?>
