78ec9048e0bb79c3e4683efb69008697f85fb109
Michael <ironbee2002@yandex.ru>
2022-05-21 19:25:02 +0700

    71 Class Form

--- CodeMu_56-78/index.php
+++ CodeMu_56-78/index.php
@@ -1,21 +1,10 @@
 <?php
-	require_once 'HtmlList.php';
-	require_once 'ClassUL.php';
-	require_once 'ClassOl.php';
-	$ul = new Ul; // сделаем список ul
-	$ol = new Ol; // сделаем список ol
-			
-	echo $ol
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		->show(); 
+	require_once 'Form.php';
 	
-	echo '<br>';
-	echo $ul
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		->show(); 
+	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'POST']);
 	
+	echo $form->open();
+		// здесь потом будут элементы формы
+	echo $form->close();
 ?>
+	
